21e5228caebd776c53c446847e16b29234e839df
aLIEzsss4 <vuetomato@gmail.com>
2022-10-31 15:59:32 +0800

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ const { chains, provider, webSocketProvider } = configureChains(
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+      apiKey: '51TT7fdiMtNYzM5YaNtmJ4FDqgdX0BdQ',
     }),
     publicProvider(),
   ]
